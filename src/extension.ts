6b1daeb2c32fd5ea0d95888aaa7d3ae6d06fcb58
kkavyaba <kkavyaba@blueplanet.com>
2026-02-04 16:43:28 +0530

    explanatory comments added before cloning

--- src/extension.ts
+++ src/extension.ts
@@ -30,6 +30,7 @@ export function activate(context: vscode.ExtensionContext) {
   //   'Congratulations, your extension "my-first-extension" is now active!',
   // );
 
+  
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
